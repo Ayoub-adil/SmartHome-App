84e306e68e92d90a340696b688040f968977dfd4
ouma77 <essemrhounioumayma@gmail.com>
2020-04-23 22:55:19 +0200

    initial commit

---
+++ src/SignIn.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { IonApp, IonHeader, IonContent, IonToolbar, IonItem, IonLabel, IonInput, IonButton, IonIcon, IonImg} from '@ionic/react';
+import key from './images/key3.png';
+
+
+/* Theme variables */
+import './theme/variables.css';
+import './theme/style.css';
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonHeader>
+      <IonToolbar>
+        <div className='ion-margin'>
+          <IonIcon name="menu-outline" size="large"></IonIcon>
+        </div>    
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className="ion-padding-top">
+      <div className="right">
+      <IonImg className='img' src={key}></IonImg>
+      </div>
+      <div className="left">
+      <IonItem>
+      <h1>Welcome to <span className="smart">SmartHome</span></h1>
+      <p>Authenticate your account</p>
+      <IonLabel position="floating">Login :</IonLabel>
+      <IonInput className="input" type="email"></IonInput>
+    </IonItem>
+    <IonItem>
+      <IonLabel position="floating">Password :</IonLabel>
+      <IonInput className="input" type="password"></IonInput>
+    </IonItem>
+    <div className='ion-padding-top ion-text-center'>
+    <IonButton shape='round' type="submit" className='btnSub ion-margin-top'>Sign up</IonButton>
+    </div>
+      </div>
+    
+    </IonContent>
+  </IonApp>
+);
+
+export default App;
